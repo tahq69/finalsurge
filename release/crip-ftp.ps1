a478cb3dd22a462324cd2b06e1923f67734e6b72
Igors Krasjukovs <tahq69@gmail.com>
2017-07-20 21:40:33 +0300

    Release script added;

---
+++ release/crip-ftp.ps1
@@ -0,0 +1,27 @@
+﻿#we specify the directory where all files that we want to upload  
+$scriptPath = $PSScriptRoot;
+$Dir = (get-item $scriptPath ).parent.FullName;
+#ftp server
+$ftp = "ftp://ftp.crip.lv/finalsurge/"
+$user = "u386704066"
+$pass = Get-Content "${PSScriptRoot}/password"
+
+$webclient = New-Object System.Net.WebClient
+
+$webclient.Credentials = New-Object System.Net.NetworkCredential($user,$pass)
+
+"Uploading /*.html"
+
+foreach($item in (dir "${Dir}/" "*.html")) {
+    " $item..."
+    $uri = New-Object System.Uri($ftp+$item.Name)
+    $webclient.UploadFile($uri, $item.FullName)
+}
+
+"Uploading /dir/*"
+
+foreach($item in (dir "${Dir}/dist/" "*.*")){
+    "Uploading /dir/ $item..."
+    $uri = New-Object System.Uri($ftp+'dist/'+$item.Name)
+    $webclient.UploadFile($uri, $item.FullName)
+}
