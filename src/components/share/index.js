d987acbe38cf8398663fdc672310366c97f238bb
Igors Krasjukovs <tahq69@gmail.com>
2017-07-24 00:22:49 +0300

    Bugfixes and share buttons implemented;

--- src/components/share/index.js
+++ src/components/share/index.js
@@ -3,7 +3,9 @@ export default new Vue({
   el: '#social-share',
   data() {
     return {
-      isVisible: true
+      isVisible: true,
+      url: encodeURIComponent('http://finalsurge.crip.lv'),
+      text: encodeURIComponent('Training and coaching platform ')
     }
   },
   methods: {
@@ -12,6 +14,22 @@ export default new Vue({
     },
     hide() {
       this.isVisible = false
+    },
+    tweet() {
+      let url = `https://twitter.com/home?status=${this.text + this.url}`
+      this.window(url)
+    },
+    facebook() {
+      let url = `https://www.facebook.com/sharer/sharer.php?u=${this.url}`
+      this.window(url)
+    },
+    linkedin() {
+      let url = `https://www.linkedin.com/shareArticle?mini=true&url=${this.url}&title=${this.text}`
+        + `&summary=${encodeURIComponent('Empowering athletes and coaches to reach fitness and performance excellence like never before.')}`
+      this.window(url)
+    },
+    window(url) {
+      window.open(url, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600')
     }
   }
 })
